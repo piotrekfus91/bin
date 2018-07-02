204b2a1d0d09743c205510d68a9941e619677fc7
Piotr Fus <piotrek.fus.91@gmail.com>
2018-07-02 18:05:03 +0200

    Add config repositories

---
+++ update-config-repositories.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+scrip_dir=$(dirname "$0")
+
+declare -a repos=(
+    "https://github.com/powerline/fonts"
+)
+
+verify_actual() {
+    repo=$1
+    repoDir=$(echo $repo | awk -F/ '{print $NF}')
+    if [ -d repos/$repoDir ] ; then
+        cd repos/$repoDir
+        git fetch
+        behind=$(git rev-list --left-right --count master...origin/master | awk '{print $2}')
+        if [ $behind -ne 0 ] ; then
+            echo "Repository $repo is not actual" >> /tmp/non-actual-repos
+        fi
+        cd -
+    else
+        cd repos
+        git clone $repo
+        cd -
+    fi
+}
+
+cd $scrip_dir
+
+mkdir -p repos
+
+rm -f /tmp/non-actual-repos
+
+for repo in "${repos[$@]}" ; do
+    verify_actual $repo
+done
