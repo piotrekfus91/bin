42326ed60c6e1a19795200d9a6e760692b8a3b9c
Piotr Fus <piotrek.fus.91@gmail.com>
2018-07-12 13:55:40 +0200

    Add user to mulssh

--- mulssh.sh
+++ mulssh.sh
@@ -4,8 +4,14 @@ SESSION=$USER
 current_pane=$TMUX_PANE
 echo $current_pane
 
+user=""
+if [ "$1" == "-u" ] ; then
+    user=$2'@'
+    shift 2
+fi
+
 for cmd in "$@" ; do
-    tmux split-window "ssh $cmd"
+    tmux split-window "ssh $user$cmd"
 done
 
 tmux kill-pane -t "$current_pane"
